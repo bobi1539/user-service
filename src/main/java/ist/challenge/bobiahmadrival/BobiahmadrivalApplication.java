2122d16bc4d5bb2e2987f08623b890fc8d8962b5
bobi.rival <bobi.rival@dwidasa.com>
2023-01-12 12:03:08 +0700

    init project

---
+++ src/main/java/ist/challenge/bobiahmadrival/BobiahmadrivalApplication.java
@@ -0,0 +1,13 @@
+package ist.challenge.bobiahmadrival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobiahmadrivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobiahmadrivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ist/challenge/bobiahmadrival/BobiahmadrivalApplicationTests.java
@@ -0,0 +1,13 @@
+package ist.challenge.bobiahmadrival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobiahmadrivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
